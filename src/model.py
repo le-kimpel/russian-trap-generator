b52e49a5f102e486eb0ab8a2873f6b80f1318984
me <lekimpel@gmail.com>
2024-07-30 18:55:59 -0400

    preprocessing step improved

---
+++ src/model.py
@@ -0,0 +1,28 @@
+import numpy
+import tensorflow as tf
+import re
+
+'''
+model.py
+
+Trains an LSTM to spit out various russian-language song lyrics. 
+'''
+
+with open ("out.txt", "r", encoding = "utf-8") as f:
+    raw_text = f.read()
+print(raw_text[0:200])
+vocab = list(set(raw_text.split(' ')))
+    
+print(vocab[0:250])
+exit()
+print("Dataset has " + str(len(vocab)) + " unique words")
+
+ids_from_words = tf.keras.layers.StringLookup(vocabulary=list(vocab),mask_token=None)
+
+words_from_ids = tf.keras.layers.StringLookup(vocabulary=ids_from_words.get_vocabulary(), invert=True, mask_token=None)
+
+#text_from_ids = tf.strings.reduce_join(chars_from_ids, axis=-1).numpy()
+
+all_ids = ids_from_words(tf.strings.unicode_split(raw_text, 'UTF-8'))
+
+i#ds_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 df = pd.read_csv("../data/archive/genius_ru_songs_70.csv")
 
@@ -7,6 +8,14 @@ lyric_col = df["lyrics"]
 '''
 Naive way: stuff all lyrics into one blob.
 '''
-with open("out.txt", "w") as f:
+
+with open("out.txt", "w", encoding="utf-8") as f:
     for lyric in lyric_col:
-        f.write(lyric)
+        pattern = re.compile(r"[^\w\n']+", re.UNICODE)
+        lyric_mod = re.sub(pattern, ' ', lyric)
+        lyric_mod = lyric_mod.replace('\n', ' ')
+        lyric_mod = lyric_mod.replace('\u2005', ' ')
+        lyric_mod = lyric_mod.replace('\u205f', ' ')
+        f.write(lyric_mod)
+        
+
