5b2c634f8d378340045791470e27ab9b1cd158a4
me <lekimpel@gmail.com>
2024-07-30 19:27:52 -0400

    update model and credit to tutorial

--- src/model.py
+++ src/model.py
@@ -1,12 +1,28 @@
-import numpy
-import tensorflow as tf
-import re
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,LSTM,Embedding
 
 '''
 model.py
 
 Trains an LSTM to spit out various russian-language song lyrics. 
 '''
+def generate_model(vocab_size, seq_len):
+    '''
+    Generates an LSTM with an Embedding layer, two LSTM layers, and two dense layers. This is the default model using the tutorial provided by:
+
+    https://medium.com/@govardhanspace/text-generation-using-lstm-with-keras-in-python-ba5e492bedfa
+    '''
+    model = Sequential()
+    model.add(Embedding(vocab_size, 25, input_length = seq_len))
+    model.add(LSTM(150, return_sequences=True))
+    model.add(LSTM(150))
+    model.add(Dense(150), activation='relu'))
+    model.add(Dense(vocab_size, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    return model
 
 with open ("out.txt", "r", encoding = "utf-8") as f:
     raw_text = f.read()
@@ -15,12 +31,13 @@ vocab = list(set(raw_text.split(' ')))
 
 print("Dataset has " + str(len(vocab)) + " unique words")
 
-ids_from_words = tf.keras.layers.StringLookup(vocabulary=list(vocab),mask_token=None)
+train_len = 25+1
+text_sequences = []
+for i in range(train_len, len(vocab)):
+    seq = vocab[i-train_len:i]
+    text_sequences.append(seq)
 
-words_from_ids = tf.keras.layers.StringLookup(vocabulary=ids_from_words.get_vocabulary(), invert=True, mask_token=None)
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(text_sequences)
+sequences = tokenizer.texts_to_sequences(text_sequences)
 
-#text_from_ids = tf.strings.reduce_join(chars_from_ids, axis=-1).numpy()
-
-all_ids = ids_from_words(tf.strings.unicode_split(raw_text, 'UTF-8'))
-
-i#ds_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
