c363df11e09d7aad5b748563174c9728cc34e85c
me <lekimpel@gmail.com>
2024-07-30 20:00:00 -0400

    mistake in train-test set

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense,LSTM,Embedding
+from tensorflow.keras.utils import to_categorical
 
 '''
 model.py
@@ -18,7 +19,7 @@ def generate_model(vocab_size, seq_len):
     model.add(Embedding(vocab_size, 25, input_length = seq_len))
     model.add(LSTM(150, return_sequences=True))
     model.add(LSTM(150))
-    model.add(Dense(150), activation='relu'))
+    model.add(Dense(150), activation='relu')
     model.add(Dense(vocab_size, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.summary()
@@ -27,17 +28,29 @@ def generate_model(vocab_size, seq_len):
 with open ("out.txt", "r", encoding = "utf-8") as f:
     raw_text = f.read()
 print(raw_text[0:200])
-vocab = list(set(raw_text.split(' ')))
+tokens = []
+clean_text = raw_text.lower()
+clean_text = clean_text.split(' ')
+for word in clean_text:
+    if word != '':
+        tokens.append(word)
 
-print("Dataset has " + str(len(vocab)) + " unique words")
+print("Dataset has " + str(len(list(set(tokens)))) + " unique words")
 
 train_len = 25+1
 text_sequences = []
-for i in range(train_len, len(vocab)):
-    seq = vocab[i-train_len:i]
+for i in range(train_len, len(tokens)):
+    seq = tokens[i-train_len:i]
     text_sequences.append(seq)
 
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(text_sequences)
 sequences = tokenizer.texts_to_sequences(text_sequences)
 
+# split vocab into train/test set
+X = sequences[:, :-1]
+y = sequences[:,-1]
+y = to_categorical(y, num_classes=vocab_size+1)
+
+# train model
+model.fit(X,y, batch_size=128, epochs=300, verbose=1)
